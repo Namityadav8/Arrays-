69a0a32eca2237726fc6f4108fd275e98f131491
Namit Yadav <114800158+Namityadav8@users.noreply.github.com>
2022-12-06 19:04:55 +0530

    this is how we use arrays in python

---
+++ arraypractise.py
@@ -0,0 +1,14 @@
+a = "namit yadav"
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])#index starts from 0 and space between words is also counted in indexes 
+print(len(a))
+
+b="i don't know where i am going and what i will do in my life "
+print(len(b)) #for checking the length of a sentence 
+if "life" in b  :
+    print("yes! it is there ")#to print a particular word using indexesc
+else:
+        print("no")
